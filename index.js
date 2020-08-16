a69d47eb0959897ec7f5d4d54aba4808335779f1
aaron-schuyler <asky28@gmail.com>
2020-08-16 17:45:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(arr, cb) {
+  let array = []
+  for (const v of arr) {
+    array.push(cb(v))
+  }
+  return array
+}
+function reduce(arr, cb, s) {
+  let total
+  if (s) {
+    total = s
+    for (const v of arr) {
+      total = cb(v, total)
+    }
+  } else {
+    total = arr[0]
+    for (const v of arr.splice(1)) {
+      total = cb(v, total)
+    }
+  }
+
+  return total
+}
